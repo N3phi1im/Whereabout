35debf0dfd6eb8e627d8b239f2338ad827488b17
Philip lanphar <pjlanphar@gmail.com>
2015-08-17 16:59:24 -0700

    working on same page navigation

--- src/javascript/Controllers/NavbarController.js
+++ src/javascript/Controllers/NavbarController.js
@@ -3,16 +3,16 @@
 	angular.module('app')
 	.controller('NavbarController', NavbarController);
 
-	NavbarController.$inject = ['UserFactory', '$state'];
+	NavbarController.$inject = ['UserFactory', '$state', '$scope','$anchorScroll', '$location'];
 
-	function NavbarController(UserFactory, $state) {
+	function NavbarController(UserFactory, $state, $scope, $anchorScroll, $location) {
 		var vm = this;
 		vm.user = {};
 		vm.status = UserFactory.status;
 		vm.register = register;
 		vm.login = login;
 		vm.logout = UserFactory.logout;
-
+		vm.scrollTo = scrollTo;
 
 		function register() {
 			var u = vm.user;
@@ -28,5 +28,12 @@
 				$state.go('Home');
 			});
 		}
+
+		function scrollTo(id) {
+			$location.hash(id);
+			console.log($location.hash());
+			$anchorScroll();
+		}
+
 	}
 })();
