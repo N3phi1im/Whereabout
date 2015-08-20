3daa8d1abb48c57f14ec8b63387fe65a341ad17c
snoopy223 <idctony@gmail.com>
2015-08-20 10:47:29 -0700

    on Homepage

---
+++ src/javascript/Controllers/CarouselCtrl.js
@@ -0,0 +1,16 @@
+angular.module('app', ['ui.bootstrap']);
+function CarouselCtrl($scope) {
+  $scope.myInterval = 5000;
+  var slides = $scope.slides = [];
+  $scope.addSlide = function() {
+    var newWidth = 600 + slides.length;
+    slides.push({
+      image: 'http://placekitten.com/' + newWidth + '/300',
+      text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+      ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+    });
+  };
+  for (var i=0; i<4; i++) {
+    $scope.addSlide();
+  }
+}
