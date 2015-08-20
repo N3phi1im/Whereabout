98a154bab94c5609bdb942fbd8d1354c6c8d5d78
snoopy223 <idctony@gmail.com>
2015-08-20 15:12:28 -0700

    carousel works

--- src/javascript/Controllers/CarouselCtrl.js
+++ src/javascript/Controllers/CarouselCtrl.js
@@ -1,11 +1,11 @@
-angular.module('app', ['ui.bootstrap']);
-function CarouselCtrl($scope) {
-  $scope.myInterval = 5000;
+angular.module('app').controller('CarouselCtrl', ['$scope', function ($scope) {
+
+  $scope.noWrapSlides = false;
   var slides = $scope.slides = [];
   $scope.addSlide = function() {
-    var newWidth = 600 + slides.length;
+    var newWidth = 600 + slides.length + 1;
     slides.push({
-      image: 'http://placekitten.com/' + newWidth + '/300',
+      image: '//placekitten.com/' + newWidth + '/300',
       text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
       ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
     });
@@ -13,4 +13,4 @@ function CarouselCtrl($scope) {
   for (var i=0; i<4; i++) {
     $scope.addSlide();
   }
-}
+}]);
