6cad2f55222d27d55bdf39ee857c8750c259d767
Isaacg11 <isaacgrey1989@gmail.com>
2015-08-24 10:57:07 -0700

    deleted business page carousel

--- src/javascript/Controllers/CarouselCtrl.js
+++ src/javascript/Controllers/CarouselCtrl.js
@@ -1,8 +1,17 @@
-angular.module('app').controller('CarouselCtrl', ['$scope', function ($scope) {
+(function() {
+  'use strict';
+  angular.module('app')
+  .controller('CarouselCtrl', CarouselCtrl);
 
-  $scope.noWrapSlides = false;
-  var slides = $scope.slides = [];
-  $scope.addSlide = function() {
+  CarouselCtrl.$inject=['HomeFactory','$state'];
+
+  function CarouselCtrl(HomeFactory,$state) {
+    var vm = this;
+  vm.noWrapSlides = false; //setting no-wrap to an expression which evaluates to a truthy value will prevent looping
+  var slides = vm.slides = []; //this is the array that will be displayed on the view
+
+
+  vm.addSlide = function() {
     var newWidth = 600 + slides.length + 1;
     slides.push({
       image: '//placekitten.com/' + newWidth + '/300',
@@ -11,6 +20,30 @@ angular.module('app').controller('CarouselCtrl', ['$scope', function ($scope) {
     });
   };
   for (var i=0; i<4; i++) {
-    $scope.addSlide();
+    vm.addSlide();
   }
-}]);
+}
+})();
+
+
+
+//-------------------------------------------------------------------//
+// $SCOPE
+
+// angular.module('app')
+// .controller('CarouselCtrl', ['$scope', function ($scope) {
+
+//   $scope.noWrapSlides = false; //
+//   var slides = $scope.slides = [];
+//   $scope.addSlide = function() {
+//     var newWidth = 600 + slides.length + 1;
+//     slides.push({
+//       image: '//placekitten.com/' + newWidth + '/300',
+//       text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+//       ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+//     });
+//   };
+//   for (var i=0; i<4; i++) {
+//     $scope.addSlide();
+//   }
+// }]);
