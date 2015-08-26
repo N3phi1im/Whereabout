0fc44e6b71a53883f4a08503ac7d68a4d7693afb
n3phi1im <christopher.johnkellogg@gmail.com>
2015-08-26 09:09:57 -0700

    Started Pull of Home page

--- src/javascript/Controllers/HomeController.js
+++ src/javascript/Controllers/HomeController.js
@@ -1,24 +1,24 @@
 (function() {
-	'use strict';
-	angular.module('app')
-	.controller('HomeController', HomeController);
+  'use strict';
+  angular.module('app')
+    .controller('HomeController', HomeController);
 
-	HomeController.$inject = ['HomeFactory','UserFactory'];
+  HomeController.$inject = ['HomeFactory', 'UserFactory'];
 
-	function HomeController(HomeFactory,UserFactory) {
-		var vm = this;
-//-----------------------------------------------------------//
-vm.upload = function(photo) {
-	HomeFactory.upload(photo).then(function() {
-		HomeFactory.setPhoto().then(function() {
-			HomeFactory.setPlace(id).then(function() {
-				state.go('Home');
-			});
-		});
-	});
-};
-	//-----------------------------------------------------------//
+  function HomeController(HomeFactory, UserFactory) {
+    var vm = this;
 
-	
-}
+    vm.upload = function(photo) {
+      HomeFactory.upload(photo).then(function() {
+        HomeFactory.setPhoto().then(function() {
+          HomeFactory.setPlace(id).then(function() {
+            state.go('Home');
+          });
+        });
+      });
+    };
+
+		// vm.populateHome = function()
+
+  }
 })();
