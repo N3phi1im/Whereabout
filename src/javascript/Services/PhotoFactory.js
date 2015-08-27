d03bc665b4d746d7b7542dffe243b8f5c8f60aa9
n3phi1im <christopher.johnkellogg@gmail.com>
2015-08-27 16:53:59 -0700

    Reset Password email support added / inprogress

--- src/javascript/Services/PhotoFactory.js
+++ src/javascript/Services/PhotoFactory.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
   angular.module('app')
-  .factory('PhotoFactory', PhotoFactory);
+    .factory('PhotoFactory', PhotoFactory);
 
   PhotoFactory.$inject = ['HomeFactory', 'Map', 'UserFactory', '$http', '$q'];
 
@@ -27,60 +27,68 @@
       });
       return q.promise;
     }
-//-------------------------------------------------------------------------//
-function  addPhoto(photo) {
-  var q = $q.defer();
-  obj.id = photo.id;
-  q.resolve();
-  return q.promise;
-}
-//-------------------------------------------------------------------------//
-function combinePhotoComment(comment) {
-  var id = obj.id;
-  var q = $q.defer();
-  $http.post('/api/Comment/add/'+ id, {comment: comment}, {
-    headers: {
-      Authorization: "Bearer " + localStorage.getItem('token')}}).success(function(res){
+    //-------------------------------------------------------------------------//
+    function addPhoto(photo) {
+      var q = $q.defer();
+      obj.id = photo.id;
+      q.resolve();
+      return q.promise;
+    }
+    //-------------------------------------------------------------------------//
+    function combinePhotoComment(comment) {
+      var id = obj.id;
+      var q = $q.defer();
+      $http.post('/api/Comment/add/' + id, {
+        comment: comment
+      }, {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem('token')
+        }
+      }).success(function(res) {
         q.resolve(res);
       });
       return q.promise;
     }
 
-//-------------------------------------------------------------------------//
-function getComment() {
-  var id = obj.id;
-  var q = $q.defer();
-  $http.get('/api/Comment/get/'+ id, 
-  {
-    headers: {
-      Authorization: "Bearer " + localStorage.getItem('token')}}).success(function(res){
+    //-------------------------------------------------------------------------//
+    function getComment() {
+      var id = obj.id;
+      var q = $q.defer();
+      $http.get('/api/Comment/get/' + id, {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem('token')
+        }
+      }).success(function(res) {
         o.comments.push.apply(o.comments, res);
         q.resolve(res);
       });
       return q.promise;
     }
-//-------------------------------------------------------------------------//
+    //-------------------------------------------------------------------------//
 
 
 
 
 
 
-//-------------------------------------------------------------------------//
+    //-------------------------------------------------------------------------//
 
 
-function myPhotos() {
-  var q = $q.defer();
-  $http.get('/api/Photos/mine', { headers: {
-    Authorization: "Bearer " + localStorage.getItem('token')}}).success(function(res) {
-      q.resolve(res);
-    });
-    return q.promise;
-}
+    function myPhotos() {
+      var q = $q.defer();
+      $http.get('/api/Photos/mine', {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem('token')
+        }
+      }).success(function(res) {
+        q.resolve(res);
+      });
+      return q.promise;
+    }
 
 
 
 
 
-}
+  }
 })();
